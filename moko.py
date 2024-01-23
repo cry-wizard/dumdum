daebb057f773f78b65ea91e8604b4e0a999b6780
cry-wizard <sidd160306@gmail.com>
2024-01-23 14:43:34 +0530

    Changes

--- moko.py
+++ moko.py
@@ -1,1 +1,9 @@
-print("Hello world")
+firstNumber = int(input("What is your first number:"))
+opertor = input("What is your sign:")
+secondNumber = int(input("What is your second number:"))
+if(opertor == "+"):{
+    print(firstNumber+secondNumber)
+}
+elif(operator == "-"):{
+    print(firstNumber-secondNumber)
+}
